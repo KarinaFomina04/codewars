490f41ccb68c3ba78475d31f23e19224e963b956
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-01-24 13:30:59 +0100

    first commit

---
+++ task1.js
@@ -0,0 +1,16 @@
+// Create a function that takes an integer as an argument and returns "Even" 
+// for even numbers or "Odd" for odd numbers.
+
+function evenOrOdd(number) {
+    if (number % 2 == 0) {
+      console.log(x = "Even")
+    } else {
+        console.log(x = "Odd");
+    }
+    return x;
+  }
+
+  evenOrOdd(3)
+  evenOrOdd(6)
+  evenOrOdd(315)
+  evenOrOdd(68)
