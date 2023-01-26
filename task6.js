386aaa63cf239a57ab4fd1b712e54794087e547e
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-01-26 14:52:18 +0100

    Add task 7

---
+++ task6.js
@@ -0,0 +1,18 @@
+// The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+
+// Your function takes three arguments:
+
+// The height of the column (meters)
+// The distance that the snail crawls during the day (meters)
+// The distance that the snail slides down during the night (meters)
+// Calculate number of day when the snail will reach the top of the column.
+
+// function snail(column, day_meter, night_meter) {
+//     const current_height = 0;
+//     const days = 0
+//     while (current_height != column) {
+//       current_height = day_meter - night_meter
+//       days += 1
+//     }
+//     return days
+//   }
---
+++ task7.js
@@ -0,0 +1,15 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    if(b){
+      return "true";
+    }else{
+      return "false";
+    }
+  }
+
+
+  booleanToString(true)
+  booleanToString(false)
