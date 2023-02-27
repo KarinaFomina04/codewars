4019f1e3e313199d5dfe2edf97be27bb6f35b11a
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-02-27 21:53:02 +0100

    Find out whether the shape is a cube

---
+++ index.js
@@ -0,0 +1,19 @@
+let s = "!hellow !world!"
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi,"")
+}
+0
+
+let arr = ["Keep", "Remove", "Keep", "Remove", "Keep"]
+function removeEveryOther(arr){
+
+}
+/*console.log( removeEveryOther(arr))*/
+
+let str = "apple ban"
+function addLength(str) {
+    return str.split(" ").map(el=> el +" "+ el.length)
+}
+
+
+
