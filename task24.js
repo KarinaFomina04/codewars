baa6432516088090b32da54de480eb8b483753e9
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-02-28 20:20:12 +0100

    Credit Card Mask

--- task24.js
+++ task24.js
@@ -1,3 +1,7 @@
+//Credit Card Mask
+
+
+
 // Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question 
 // is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 // Your task is to write a function maskify, which changes all but the last four characters into '#'.
@@ -7,16 +11,8 @@ function maskify(str) {
     const replacer = "#";
     const len = 4;
     if (str.length > 4) {
-        if (len < 0) {
-            result = str.slice(0, -len) + replacer.repeat(str.length + len)
-            console.log(rep = replacer.repeat(str.length + len))
-            console.log(sli = str.slice(0, -len))
-        }
-        else
-            result = replacer.repeat(str.length - len) + str.slice(-len);
-        return result;
+        return replacer.repeat(str.length - len) + str.slice(-len);
     }
- 
         return str
 }
 
