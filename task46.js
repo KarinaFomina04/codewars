02fd44a3ba3134918bab98a41caedc8b47c39e43
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-04-19 00:10:01 +0200

    Quadrants

--- task46.js
+++ task46.js
@@ -2,14 +2,18 @@
 //Given a point in a Euclidean plane (x and y), return the quadrant the point exists in: 1, 2, 3 or 4 (integer).
 // x and y are non-zero integers, therefore the given point never lies on the axes.
 
+// function quadrant(x, y) {
+//     if (x > 0 && y > 0) {
+//         return 1;
+//     } else if (x < 0 && y > 0) {
+//         return 2;
+//     } else if (x < 0 && y < 0) {
+//         return 3;
+//     } else {
+//         return 4;
+//     }
+// }
+
 function quadrant(x, y) {
-    if (x > 0 && y > 0) {
-        return 1;
-    } else if (x < 0 && y > 0) {
-        return 2;
-    } else if (x < 0 && y < 0) {
-        return 3;
-    } else {
-        return 4;
-    }
+    return ["11","-11","-1-1","1-1"].indexOf(`${Math.sign(x)}${Math.sign(y)}`) + 1;
 }
