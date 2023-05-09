2f4f10e0f6fd9940df33c95e0d43755e6f273e76
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-05-09 22:24:31 +0200

    5 without numbers !!

---
+++ task63.js
@@ -0,0 +1,8 @@
+//5 without numbers !!
+//Sounds easy right? Just bear in mind that you can't use any of the following
+// characters: 0123456789*+-/
+
+
+function unusualFive() {
+    return "hello".length;
+}
