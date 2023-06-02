a1fc48f0ea23242b89e513256046740f70620e52
KarinaFomina04 <karinafomina.19970804@gmail.com>
2023-06-02 14:47:20 +0200

    Grasshopper - Messi Goals

--- task70.js
+++ task70.js
@@ -2,10 +2,8 @@
 
 //Messi's Goal Total
 // Use variables to find the sum of the goals Messi scored in 3 competitions
-//
 // Information
 // Messi goal scoring statistics:
-//
 // Competition	Goals
 // La Liga	43
 // Champions League	10
